c5dc119073b304c2c5709d5b865facc01287f4b4
2021ra <87912422+2021ra@users.noreply.github.com>
2022-10-04 18:06:53 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import './App.css';
+import Blank from './Component/Blank';
+import Footer from './Component/footer';
+import Form from './Component/form';
+import Navbar from './Component/Navbar';
+import Overlap from './Component/Overlap';
+import Page_1 from './Component/Page_1';
+import Page_2 from './Component/Page_2';
+import Page_3 from './Component/Page_3';
+import Page_4 from './Component/Page_4';
+import Page_5 from './Component/Page_5';
+import Page_6 from './Component/Page_6';
+import Page_7 from './Component/Page_7';
+import Page_8 from './Component/Page_8';
+import Page_9 from './Component/Page_9';
+
+function App() {
+  return (
+    <>
+      <Navbar/>
+      <Page_1/>
+      <Overlap/>
+      <Blank/>
+      <Page_2/>
+      <Page_3/>
+      <Page_4/>
+      <Page_5/>
+      <Page_6/>
+      <Page_7/>
+      <Page_8/>
+      <Page_9/>
+      <Form/>
+      <Footer/>
+    </>
+  );
+}
+
+export default App;
